8427fb942d6e88238088e577635854f07884cb73
Leclercq David <93372601+XhaiiCo@users.noreply.github.com>
2023-01-16 10:27:28 +0100

    Refactor UtilisateurControl

--- src/main/java/be/helha/aemt/groupea1/control/UtilisateurControl.java
+++ src/main/java/be/helha/aemt/groupea1/control/UtilisateurControl.java
@@ -15,33 +15,34 @@ import jakarta.servlet.http.HttpServletRequest;
 @Named
 @SessionScoped
 public class UtilisateurControl implements Serializable {
-	
+
 	@EJB
 	private UtilisateurEJB utilisateurEJB ;
-	
-	private Utilisateur utilisateur;
-		
+
+	private Utilisateur connectedUser;
+	public Utilisateur getConnectedUser() {return this.connectedUser ;} 
+
 	@PostConstruct
 	public void init() {
-		this.currentUserInfos();
+		this.fetchConnectedUser();
 	}
-	
-	private void currentUserInfos() {
-		
+
+	private void fetchConnectedUser() {
+
 		/*get the username of the currently logged user
 		the name is the email as defined in the "User Name Column" in the Payara Realm*/
 		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
-		 
+
 		String email = request.getUserPrincipal().getName();
-		
+
 		try 
 		{
 			Utilisateur u = new Utilisateur(email);
-			
-			this.utilisateur = this.utilisateurEJB.find(u) ;
-			
+
+			this.connectedUser = this.utilisateurEJB.find(u) ;
+
 			//empty the password so it is impossible to get the hash from here
-			this.utilisateur.setPassword("");
+			this.connectedUser.setPassword("");
 		} 
 		catch (InvalidEmailException e) 
 		{
@@ -49,12 +50,4 @@ public class UtilisateurControl implements Serializable {
 			e.printStackTrace();
 		}
 	}
-
-	public Utilisateur getUtilisateur() 
-	{
-		return utilisateur;
-	}
-	
-	
-	
 }
