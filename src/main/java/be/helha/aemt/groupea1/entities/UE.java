36e1d2d3813afc14b61aec641e604b767179ab7a
VicGuillaume <67600842+VicGuillaume@users.noreply.github.com>
2023-01-16 14:34:44 +0100

    Add getters and setters in UE class

--- src/main/java/be/helha/aemt/groupea1/entities/UE.java
+++ src/main/java/be/helha/aemt/groupea1/entities/UE.java
@@ -32,5 +32,78 @@ public class UE implements Serializable {
 	private List<AA> aas ;
 	
 	public UE() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAcademicYear() {
+		return academicYear;
+	}
+
+	public void setAcademicYear(String academicYear) {
+		this.academicYear = academicYear;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public Section getSection() {
+		return section;
+	}
+
+	public void setSection(Section section) {
+		this.section = section;
+	}
+
+	public String getBloc() {
+		return bloc;
+	}
+
+	public void setBloc(String bloc) {
+		this.bloc = bloc;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getEntitled() {
+		return entitled;
+	}
+
+	public void setEntitled(String entitled) {
+		this.entitled = entitled;
+	}
+
+	public int getCredit() {
+		return credit;
+	}
+
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+
+	public List<AA> getAas() {
+		return aas;
+	}
+
+	public void setAas(List<AA> aas) {
+		this.aas = aas;
+	}
+	
 }
 
