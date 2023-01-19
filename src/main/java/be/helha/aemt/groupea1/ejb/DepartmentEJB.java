f21205604c4871dd7d9b4a6698d4dac448c0b9a4
VicGuillaume <67600842+VicGuillaume@users.noreply.github.com>
2023-01-19 15:00:46 +0100

    Add findAll method in DepartmentEJB

--- src/main/java/be/helha/aemt/groupea1/ejb/DepartmentEJB.java
+++ src/main/java/be/helha/aemt/groupea1/ejb/DepartmentEJB.java
@@ -1,8 +1,10 @@
 package be.helha.aemt.groupea1.ejb;
 
+import java.util.List;
 
 import be.helha.aemt.groupea1.dao.DepartmentDAO;
 import be.helha.aemt.groupea1.entities.Department;
+import be.helha.aemt.groupea1.entities.Utilisateur;
 import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
 
@@ -14,6 +16,10 @@ public class DepartmentEJB {
 	
 	public Department add(Department department) {
 		return departmentDAO.add(department) ;
+	}
+
+	public List<Department> findAll() {
+		return departmentDAO.findAll();
 	}
 	
 }
