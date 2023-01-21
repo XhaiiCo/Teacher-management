40116e602e3bb67bc7968968ad7d6319c8112025
VicGuillaume <67600842+VicGuillaume@users.noreply.github.com>
2023-01-21 23:04:24 +0100

    Add assignment column in MissionCRUD

--- src/main/java/be/helha/aemt/groupea1/entities/Mission.java
+++ src/main/java/be/helha/aemt/groupea1/entities/Mission.java
@@ -35,6 +35,8 @@ public abstract class Mission implements Serializable {
 	@ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.EAGER)
 	private List<Teacher> teachers = new ArrayList<>();
 
+	private String assigned = "Pas attribuée";
+
 	public Mission (){
 	}
 
@@ -43,6 +45,7 @@ public abstract class Mission implements Serializable {
 		this.entitled = entitled;
 		setHours(hours);
 		setTeachers(teachers);
+		this.setAssigned();
 	}
 	
 	public Mission(String academicYear, String entitled, int hours) throws InvalidHoursException {
@@ -50,6 +53,18 @@ public abstract class Mission implements Serializable {
 		this.entitled = entitled;
 		setHours(hours);
 		this.teachers = new ArrayList<Teacher>();
+		this.setAssigned();
+	}
+
+	public String getAssigned() {
+		return assigned;
+	}
+
+	public void setAssigned() {
+		if (this.teachers.isEmpty())
+			this.assigned = "Non attribuée";
+		else
+			this.assigned = "Attribuée";
 	}
 	
 	public abstract String getType();
@@ -96,12 +111,14 @@ public abstract class Mission implements Serializable {
 	public boolean addTeacher(Teacher teacher) {
 		if(this.teachers.contains(teacher))
 			return false ;
-
+		this.assigned = "Attribuée";
 		return this.teachers.add(teacher) ;
 	}
 	
 	public void removeTeacher(Teacher teacher) {
 		this.teachers.remove(teacher);
+		if (this.teachers.isEmpty())
+			this.assigned = "Non attribuée";
 	}
 
 	public void setTeachers(List<Teacher> teachers) {
